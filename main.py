404f455399fefa3d85776cbf7d2dadaf9bc3bbe7
Jonghwa <whdghk3471@gmail.com>
2026-04-22 09:09:29 +0900

    #2.2 Countries Table part One

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from dash import Dash, html, dcc
-import plotly.express as px
-import pandas as pd
+from dash import Dash, html
+from data import countries_df
+
+print(countries_df.values)
 
 stylesheets = [
     "https://cdn.jsdelivr.net/npm/reset-css@5.0.2/reset.min.css",
@@ -20,7 +21,40 @@ app.layout = html.Div(
         html.Header(
             style={"textAlign": "center", "paddingTop": "50px"},
             children=html.H1("Corona Dashboard", style={"fontSize": 40}),
-        )
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Table(
+                            children=[
+                                html.Thead(
+                                    children=[
+                                        html.Tr(
+                                            children=[
+                                                html.Th(column_name.replace("_", " "))
+                                                for column_name in countries_df.columns
+                                            ]
+                                        )
+                                    ]
+                                ),
+                                html.Tbody(
+                                    children=[
+                                        html.Tr(
+                                            children=[
+                                                html.Td(value_column)
+                                                for value_column in value
+                                            ]
+                                        )
+                                        for value in countries_df.values
+                                    ]
+                                ),
+                            ]
+                        )
+                    ]
+                )
+            ]
+        ),
     ],
 )
 
